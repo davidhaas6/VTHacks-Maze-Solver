df4132d227f805f3145094291758c9701bcc0022
David Haas <davidhaas6@gmail.com>
2018-07-05 16:36:44 -0400

    Updated gitignore

---
+++ app/src/main/java/com/davidhaas/mazesolver/Pointer.java
@@ -0,0 +1,14 @@
+package com.davidhaas.mazesolver;
+
+import android.view.View;
+
+/**
+ * Name: Dot
+ * Purpose: Selection dots for the bounds of the maze
+ * Author: David Haas
+ * Created: 7/2/18
+ */
+
+public class StartEndArrow extends View {
+    
+}
