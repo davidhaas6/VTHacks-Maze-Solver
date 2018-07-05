f3166a195bf9c54cb2b8a25bcc6464ad1bdc387b
David Haas <davidhaas6@gmail.com>
2018-07-05 16:32:07 -0400

    Perfected maze finding, fixed overlay viewing.

--- app/src/main/java/com/davidhaas/mazesolver/SolutionActivity.java
+++ app/src/main/java/com/davidhaas/mazesolver/SolutionActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.net.Uri;
@@ -21,10 +22,15 @@ import android.widget.Toast;
 import com.davidhaas.mazesolver.pathfinding.Asolution;
 
 import org.opencv.android.Utils;
+import org.opencv.core.Core;
 import org.opencv.core.CvException;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.Point;
 import org.opencv.core.Rect;
+import org.opencv.core.RotatedRect;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
@@ -32,16 +38,19 @@ import org.opencv.imgproc.Imgproc;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.IntBuffer;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Stack;
+import java.util.function.ObjIntConsumer;
 
 /**
  * Name: SolutionActivity
  * Purpose: Displays the solution of the maze.
  * Author: David Haas
- * Last updated: 6/28/18
+ * Created: 6/28/18
  */
 public class SolutionActivity extends Activity {
-    // TODO: Transfer drawing solution over here
     // TODO: Add loading button while program is solving maze
     // TODO: Look into this for cropping ur image: https://goo.gl/Qh8THg
     // TODO: Remove OpenCV dependencies https://developer.android.com/reference/android/graphics/Matrix
@@ -81,9 +90,9 @@ public class SolutionActivity extends Activity {
         if (image.getWidth() > image.getHeight())
             image = rotateBitmap(image, 90);
 
-        getCroppedMatrix(corners);
+        //getCroppedMatrix(corners);
 
-        //solveMaze();
+        solveMaze();
         findViewById(R.id.loadingPanel).setVisibility(View.GONE);
 
     }
@@ -125,26 +134,51 @@ public class SolutionActivity extends Activity {
 
         Imgproc.cvtColor(img_matrix.clone(), img_matrix, Imgproc.COLOR_RGB2GRAY);
 
-        //Imgproc.cvtColor(img_matrix.clone(), img_matrix, Imgproc.COLOR_RGB2GRAY);
-
         img_matrix = CVUtils.cropQuadrilateral(img_matrix, corners);
 
-        Imgproc.GaussianBlur(img_matrix, img_matrix, new Size(7, 7), 0);
+        Imgproc.GaussianBlur(img_matrix, img_matrix, new Size(1, 1), 0);
 
-        Size dstSize = new Size(img_matrix.width() / 5, img_matrix.height() / 5);
-        Log.i(TAG, "getCroppedMatrix: new size: " + dstSize);
-        Mat dst = new Mat();
-        Imgproc.resize(img_matrix, dst, dstSize, 1,1, Imgproc.INTER_NEAREST);
-
-        //displayMat(dst);
         //TODO: consider making blockSize and C based off of image size?
         Imgproc.adaptiveThreshold(img_matrix, img_matrix, 255, Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY, 25, 30);
+
+        List<MatOfPoint> mazePerim = CVUtils.largest2PerimContours(img_matrix);
+        if (mazePerim != null) {
+            ArrayList<Rect> rects = new ArrayList<>();
+            for (MatOfPoint c : mazePerim)
+                rects.add(Imgproc.boundingRect(c));
+            //displayMatCnts(img_matrix, mazePerim);
+
+
+            Rect combined = CVUtils.combineRects(rects.get(0), rects.get(1));
+            rects.clear();
+            rects.add(combined);
+
+            //displayMatRects(img_matrix, rects);
+            int[][] bounds = new int[][]{
+                    {combined.x, combined.y},
+                    {combined.x + combined.width, combined.y},
+                    {combined.x + combined.width, combined.y + combined.height},
+                    {combined.x, combined.y + combined.height}
+            };
+
+            img_matrix = CVUtils.cropQuadrilateral(img_matrix, bounds);
+            //displayMat(img_matrix);
+        }
+
+
+        Size dstSize = new Size(img_matrix.width() / 2, img_matrix.height() / 2);
+        Log.i(TAG, "getCroppedMatrix: new size: " + dstSize);
+        Mat dst = new Mat();
+        Imgproc.resize(img_matrix, dst, dstSize, 1, 1, Imgproc.INTER_AREA);
+        img_matrix = dst;
+
+        //TODO: consider making blockSize and C based off of image size?
+        Imgproc.adaptiveThreshold(img_matrix, img_matrix, 255, Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY, 25, 30);
+
         displayMat(img_matrix);
+        image = mat2BMP(img_matrix);
 
         int[][] arr = CVUtils.getBinaryArray(img_matrix);
-        //Log.i(TAG, "getCroppedMatrix: arr: + \n" + printArr(arr));
-
-        //displayMat(img_matrix);
 
         return arr;
     }
@@ -177,39 +211,68 @@ public class SolutionActivity extends Activity {
                 mazetrix[i][j] = color;
             }
         }
-
         int[] pixels = get1DArray(mazetrix, 1);
 
-        Bitmap solution = Bitmap.createBitmap(mazetrix[0].length, mazetrix.length, Bitmap.Config.ARGB_8888);
-        // vector is your int[] of ARGB
-        solution.copyPixelsFromBuffer(IntBuffer.wrap(pixels));
+        Bitmap solution = Bitmap.createBitmap(pixels, mazetrix[0].length, mazetrix.length, Bitmap.Config.ARGB_8888);
 
-        //TODO: skewed solution isn't drawing
-        Rect boundingRect = CVUtils.getBoundingRect(corners);
-
-        putOverlay(image, solution, boundingRect.x, boundingRect.y);
-        //putOverlay(image, skewed_solution, corners[0][0], corners[0][1]);
+        putOverlay(image, solution, 0,0);
         imageView.setImageBitmap(image);
+
     }
 
-    public void putOverlay(Bitmap bitmap, Bitmap overlay, int x, int y) {
-        Canvas canvas = new Canvas(bitmap);
-        Paint paint = new Paint(Paint.FILTER_BITMAP_FLAG);
-        canvas.drawBitmap(overlay, x, y, paint);
+    private void putOverlay(Bitmap base, Bitmap overlay, int x, int y) {
+        Canvas canvas = new Canvas(base);
+        canvas.drawBitmap(overlay, x, y, null);
     }
 
-    public void displayMat(Mat mat) {
-        Bitmap bmp;
+
+    private void displayMat(Mat mat) {
+        Bitmap bmp = mat2BMP(mat);
+        if (bmp != null)
+            imageView.setImageBitmap(bmp);
+        else
+            Log.e(TAG, "displayMat: Bitmap is null!");
+    }
+
+    private Bitmap mat2BMP(Mat mat) {
+        Bitmap bmp = null;
 
         Mat tmp = new Mat(mat.height(), mat.width(), CvType.CV_8U, new Scalar(4));
         try {
-            Imgproc.cvtColor(mat, tmp, Imgproc.COLOR_GRAY2RGBA, 4);
+            if (mat.channels() == 1)
+                Imgproc.cvtColor(mat, tmp, Imgproc.COLOR_GRAY2RGBA, 4);
             bmp = Bitmap.createBitmap(tmp.cols(), tmp.rows(), Bitmap.Config.ARGB_8888);
             Utils.matToBitmap(tmp, bmp);
-            imageView.setImageBitmap(bmp);
         } catch (CvException e) {
             Log.d("Exception", e.getMessage());
         }
+
+        return bmp;
+    }
+
+    public void displayMatCnts(Mat mat, List<MatOfPoint> contours) {
+        Mat tmp = new Mat(mat.height(), mat.width(), CvType.CV_8U, new Scalar(4));
+
+        if (mat.channels() == 1)
+            Imgproc.cvtColor(mat, tmp, Imgproc.COLOR_GRAY2RGBA, 4);
+        Imgproc.drawContours(tmp, contours, -1, new Scalar(0, 255, 0, 255), 1);
+
+        displayMat(tmp);
+    }
+
+    public void displayMatRects(Mat mat, List<Rect> rects) {
+        Mat tmp = new Mat(mat.height(), mat.width(), CvType.CV_8U, new Scalar(4));
+
+        if (mat.channels() == 1)
+            Imgproc.cvtColor(mat, tmp, Imgproc.COLOR_GRAY2RGBA, 4);
+
+        for (Rect r : rects) {
+            Point p1 = new Point(r.x, r.y);
+            Point p2 = new Point(r.x + r.width, r.y + r.height);
+            Core.rectangle(tmp, p1, p2, new Scalar(255, 0, 0, 255));
+        }
+
+        displayMat(tmp);
     }
 
     public String printArr(int[][] arr) {
